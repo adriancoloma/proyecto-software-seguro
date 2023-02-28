b359217a8c2392f99eb119de7db48a297bb108d5
adriancoloma <carlos.coloma@epn.edu.ec>
2023-02-27 22:33:20 -0500

    Primer diseño de pagina

---
+++ react-nextjs/utils/encuesta9.ts
@@ -0,0 +1,191 @@
+export default {
+  "title": "Encuesta de seguridad para proveedores subcontratados",
+  "completedHtml": "<h4>Gracias por completar la encuesta</h4>",
+  "pages": [
+    {
+      "name": "page1",
+      "elements": [
+        {
+          "type": "radiogroup",
+          "name": "pregunta1",
+          "title": "1. ¿Tiene su proveedor una política de seguridad de la información?",
+          "isRequired": true,
+          "choices": [
+            {
+              "value": "Sí",
+              "text": "Sí"
+            },
+            {
+              "value": "No",
+              "text": "No"
+            },
+            {
+              "value": "No estoy seguro/a",
+              "text": "No estoy seguro/a"
+            }
+          ]
+        },
+        {
+          "type": "radiogroup",
+          "name": "pregunta2",
+          "title": "2. ¿Tienen sus empleados acceso a la información confidencial de sus clientes?",
+          "isRequired": true,
+          "choices": [
+            {
+              "value": "Sí",
+              "text": "Sí"
+            },
+            {
+              "value": "No",
+              "text": "No"
+            },
+            {
+              "value": "No estoy seguro/a",
+              "text": "No estoy seguro/a"
+            }
+          ]
+        },
+        {
+          "type": "radiogroup",
+          "name": "pregunta3",
+          "title": "3. ¿Tiene su proveedor un sistema de control de acceso a la información?",
+          "isRequired": true,
+          "choices": [
+            {
+              "value": "Sí",
+              "text": "Sí"
+            },
+            {
+              "value": "No",
+              "text": "No"
+            },
+            {
+              "value": "No estoy seguro/a",
+              "text": "No estoy seguro/a"
+            }
+          ]
+        },
+        {
+          "type": "radiogroup",
+          "name": "pregunta4",
+          "title": "4. ¿Tiene su proveedor un sistema de gestión de contraseñas seguro?",
+          "isRequired": true,
+          "choices": [
+            {
+              "value": "Sí",
+              "text": "Sí"
+            },
+            {
+              "value": "No",
+              "text": "No"
+            },
+            {
+              "value": "No estoy seguro/a",
+              "text": "No estoy seguro/a"
+            }
+          ]
+        },
+        {
+          "type": "radiogroup",
+          "name": "pregunta5",
+          "title": "5. ¿Realizan sus empleados sesiones de formación sobre seguridad de la información?",
+          "isRequired": true,
+          "choices": [
+            {
+              "value": "Sí",
+              "text": "Sí"
+            },
+            {
+              "value": "No",
+              "text": "No"
+            },
+            {
+              "value": "No estoy seguro/a",
+              "text": "No estoy seguro/a"
+            }
+          ]
+        },
+        {
+          "type": "radiogroup",
+          "name": "pregunta6",
+          "title": "6. ¿Tiene su proveedor un sistema de gestión de incidencias de seguridad?",
+          "isRequired": true,
+          "choices": [
+            {
+              "value": "Sí",
+              "text": "Sí"
+            },
+            {
+              "value": "No",
+              "text": "No"
+            },
+            {
+              "value": "No estoy seguro/a",
+              "text": "No estoy seguro/a"
+            }
+          ]
+        },
+        {
+          "type": "radiogroup",
+          "name": "pregunta7",
+          "title": "7. ¿Tiene su proveedor un sistema de gestión de vulnerabilidades de seguridad?",
+          "isRequired": true,
+          "choices": [
+            {
+              "value": "Sí",
+              "text": "Sí"
+            },
+            {
+              "value": "No",
+              "text": "No"
+            },
+            {
+              "value": "No estoy seguro/a",
+              "text": "No estoy seguro/a"
+            }
+          ]
+        },
+        {
+          "type": "radiogroup",
+          "name": "pregunta8",
+          "title": "8. ¿Tiene su proveedor un sistema de gestión de parches de seguridad?",
+          "isRequired": true,
+          "choices": [
+            {
+              "value": "Sí",
+              "text": "Sí"
+            },
+            {
+              "value": "No",
+              "text": "No"
+            },
+            {
+              "value": "No estoy seguro/a",
+              "text": "No estoy seguro/a"
+            }
+          ]
+        },
+        {
+          "type": "radiogroup",
+          "name": "pregunta9",
+          "title": "9. ¿Tiene su proveedor un sistema de gestión de respaldos de seguridad?",
+          "isRequired": true,
+          "choices": [
+            {
+              "value": "Sí",
+              "text": "Sí"
+            },
+            {
+              "value": "No",
+              "text": "No"
+            },
+            {
+              "value": "No estoy seguro/a",
+              "text": "No estoy seguro/a"
+            }
+          ]
+        }
+      ]
+    }
+    ]
+}
