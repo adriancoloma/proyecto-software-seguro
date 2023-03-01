0e6a5b6473a8b4c2b48edfc2956e443c90e68c9a
Jeffers1998 <jefferson.alquinga@epn.edu.ec>
2023-02-28 19:23:42 -0500

    PUNTO5
    Punto 5 terminado

--- react-nextjs/pages/index.tsx
+++ react-nextjs/pages/index.tsx
@@ -1,11 +1,23 @@
 import Link from 'next/link'
 import Layout from '../components/Layout'
+import Punto1 from '../components/Punto1.1';
+import Punto5 from '../components/punto5/Punto5';
+import Punto8 from '../components/punto8/Punto8';
 
 const IndexPage = () => (
   <Layout title="Home | Next.js + TypeScript Example">
     <h1>Hello Next.js 👋</h1>
     <p>
       <Link href="/about">About</Link>
+    </p>
+    <p>
+      <Link href="/Punto1.1">Punto1.1</Link>
+    </p>
+    <p>
+      <Link href="/punto5">Punto5</Link>
+    </p>
+    <p>
+      <Link href="/punto8">Punto8</Link>
     </p>
   </Layout>
 )
