a386a96a30af5fd62b67bcaa440c1c8a25a979ed
Ariel Marcelo <55715664+Ariel-Marcelo@users.noreply.github.com>
2023-03-01 19:33:05 -0500

    update kaos metodology

--- react-nextjs/components/punto1/Punto1.3.tsx
+++ react-nextjs/components/punto1/Punto1.3.tsx
@@ -1,23 +1,16 @@
-import React, {useEffect} from 'react';
-import AccordeonDescripcion from "../AccordeonDescripcion";
-import {IAcordeon} from "../../interfaces/IAcordeon";
-import Acordion from "../Acordion";
-import dynamic from "next/dynamic";
-
+import React from "react";
+import Accordion from "../Acordion";
 export default function Punto1_3() {
-    let datosAcordeon: IAcordeon = {
-        titulo1: "Control",
-        descripcion1: "La prueba de la funcionalidad de seguridad debe llevarse a cabo durante el desarrollo.",
-        titulo2: "Guía de implementación",
-        descripcion2: "Los sistemas nuevos y actualizados requieren pruebas y verificaciones exhaustivas durante los procesos de desarrollo, incluida la preparación de un cronograma detallado de actividades y entradas de prueba y resultados esperados bajo una variedad de condiciones. Para los desarrollos internos, estas pruebas deben ser realizadas inicialmente por el equipo de desarrollo. Luego se deben realizar pruebas de aceptación independientes (tanto para desarrollos internos como externos) para garantizar que el sistema funcione como se espera y solo como se espera (ver 14.1.1 y 14.1.9). El alcance de las pruebas debe ser proporcional a la importancia y naturaleza del sistema."
-    }
-    const D_Formulario8 = dynamic(() => import('./Formulario1.3'), {ssr: false});
     return (
-        <>
-        <Acordion title="C) Requisitos de seguridad en la fase de diseño">
-            <D_Formulario8/>
-        </Acordion>
-            
-        </>
-    )
+        <div>
+            <Accordion title="C) Requisitos de seguridad en la fase de diseño">
+                <div>
+                    <p><strong>KAOS (Knowledge Acquisition in automated Specification)</strong> es una metodología de ingeniería de requisitos que se enfoca en la adquisición de conocimiento y la especificación automatizada de requisitos de software.</p>
+                    <p>En KAOS, la seguridad se considera como un atributo del sistema que debe ser analizado y especificado de manera sistemática. Para ello, se identifican los agentes que pueden afectar la seguridad del sistema, se modelan los objetivos de seguridad que se deben lograr y se especifican los requisitos necesarios para alcanzar esos objetivos.</p>
+                    <p>Además, KAOS utiliza un enfoque de refinamiento de objetivos para especificar los requisitos de seguridad de manera más detallada y para identificar los componentes del sistema que deben ser protegidos. Este enfoque permite establecer una cadena de objetivos y subobjetivos de seguridad que se deben alcanzar para garantizar la seguridad del sistema en su conjunto.</p>
+                    <p>KAOS es especialmente útil para proyectos que requieran un alto nivel de seguridad o son bastante complejos, sin embargo si se trata de un proyecto simple se puede proceder a  identificar los principales riesgos y amenazas a la seguridad y se especifican los requisitos de seguridad necesarios para mitigarlos, sin necesidad de utilizar el enfoque de refinamiento de objetivos que se utiliza en proyectos más grandes y complejos.</p>
+                </div>
+            </Accordion>
+        </div>
+    );
 }
--- react-nextjs/components/punto1/Punto1.7.tsx
+++ react-nextjs/components/punto1/Punto1.7.tsx
@@ -6,6 +6,7 @@ export default function Punto1_7() {
         <div>
             <Accordion title="G) Conocimientos de seguridad y aplicaciones requeridos">
                 <p>Se debe contar con personal con una experiencia mínima de 3 años en el desarrollo de aplicaciones seguras afines al sistema software que se esté realizando.</p>
+                <p>El personal encargado de la seguridad debe ser incentivado y de ser necesario asistido para que sea capaz de compartir su conocimiento con los demás miembros de la organización ya sea por distintos medios de comunicación, oral, escrito o visual.</p>
             </Accordion>
         </div>
     );
